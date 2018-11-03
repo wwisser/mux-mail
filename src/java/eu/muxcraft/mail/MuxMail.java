a1df074db6ce8e2705afa112ab2b8f3b08ebaa08
Wende2k <wisserwendelin@gmail.com>
2018-11-03 16:39:20 +0100

    Implemented MailCommand, adapted plugin launcher

--- src/java/eu/muxcraft/mail/MuxMail.java
+++ src/java/eu/muxcraft/mail/MuxMail.java
@@ -1,31 +1,30 @@
 package eu.muxcraft.mail;
 
+import eu.muxcraft.mail.command.MailCommand;
 import eu.muxcraft.mail.inventory.MailInventory;
 import eu.muxcraft.mail.service.MailService;
 import eu.muxcraft.mail.service.impl.LocalMailService;
-import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.command.Command;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 @Plugin(name = "MuxMail", version = "0.1-SNAPSHOT")
 @Author(name = "Wende2k")
+@Command(name = "mail")
 @Description(desc = "Allows you to send offline mail-messages.")
 public class MuxMail extends JavaPlugin {
 
-    @Getter
-    private static MailService mailService;
-
-    @Getter
-    private static MailInventory mailInventory;
-
     @Override
     public void onEnable() {
-        MuxMail.mailService = new LocalMailService();
-        MuxMail.mailInventory = new MailInventory(MuxMail.mailService);
+        MailService mailService = new LocalMailService();
+        MailInventory mailInventory = new MailInventory(mailService);
 
-        super.getServer().getPluginManager().registerEvents(MuxMail.mailInventory, this);
+        super.getServer().getPluginManager().registerEvents(mailInventory, this);
+        super.getCommand("mail").setExecutor(
+            new MailCommand(mailService, mailInventory)
+        );
     }
 
 }
--- src/java/eu/muxcraft/mail/command/MailCommand.java
+++ src/java/eu/muxcraft/mail/command/MailCommand.java
@@ -1,13 +1,69 @@
 package eu.muxcraft.mail.command;
 
+import eu.muxcraft.mail.inventory.MailInventory;
+import eu.muxcraft.mail.model.MailItem;
+import eu.muxcraft.mail.service.MailService;
+import lombok.AllArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
+
+@AllArgsConstructor
 public class MailCommand implements CommandExecutor {
+
+    private static final String USAGE = "§cVerwendung: /mail <view|send> <target> <message>";
+
+    private MailService mailService;
+    private MailInventory mailInventory;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Dieser Command ist nur für Spieler.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length < 1) {
+            sender.sendMessage(USAGE);
+            return true;
+        }
+
+        if (args.length == 1 && args[0].equalsIgnoreCase("view")) {
+            player.openInventory(this.mailInventory.createInventory(player));
+        } else {
+            sender.sendMessage(USAGE);
+        }
+
+        if (args.length >= 3 && args[0].equalsIgnoreCase("send")) {
+            String target = args[1];
+            String message = StringUtils.join(Arrays.copyOfRange(args, 2, args.length), " ");
+
+            if (!this.mailService.existsUser(target)) {
+                player.sendMessage("§cZiel '" + target + "' existiert nicht.");
+                return true;
+            }
+
+            this.mailService.add(target, new MailItem(
+                UUID.randomUUID(),
+                player.getName(),
+                target,
+                new Date(),
+                message
+            ));
+
+            player.sendMessage("§aDeine Mail wurde abgeschickt!");
+        } else {
+            sender.sendMessage(USAGE);
+        }
+
         return true;
     }
 
